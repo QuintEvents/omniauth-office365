53521875eebcbd83672e8cc1a19b9ad6bd4fb16a
Nick Merwin <n@mer.io>
2016-12-14 10:38:21 -0800

    Overwrite callback_url to ignore query string params

--- lib/omniauth/strategies/office365.rb
+++ lib/omniauth/strategies/office365.rb
@@ -34,6 +34,10 @@ module OmniAuth
       def raw_info
         @raw_info ||= access_token.get(authorize_params.resource + 'Me?api-version=1.5').parsed
       end
+      
+      def callback_url
+        options[:redirect_uri] || (full_host + script_name + callback_path)
+      end
     end
   end
 end
